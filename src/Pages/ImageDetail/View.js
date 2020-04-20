dee82b4dcb77fa49d84652d0543208e471c359e0
unknown <dandan3029@mail.dlut.edu.cn>
2020-04-20 22:40:06 +0800

    add: add the download image function

--- src/Pages/ImageDetail/View.js
+++ src/Pages/ImageDetail/View.js
@@ -24,6 +24,7 @@ class ImageDetail extends React.Component {
         const {imageId} = this.props.match.params;
         const imageInfo = {
             imageId: '0',
+            imageName: '1.png',
             imageSrc: ImageSrc,
             height: 400,
             width: 500,
@@ -39,6 +40,36 @@ class ImageDetail extends React.Component {
         if(value !== '') {
             this.props.history.push(`${PAGE_ID_TO_ROUTE[PAGE_ID.RETRIEVALRESULT]}/${value}`);
         }
+    }
+
+    downloadImage(src, name) {
+        // 调用方式
+        // 参数一： 图片的URL
+        // 参数二： 图片名称，可选
+        const image = new Image();
+        // 解决跨域 canvas 污染问题
+        image.setAttribute("crossOrigin", "anonymous");
+        image.onload = function() {
+          const canvas = document.createElement("canvas");
+          canvas.width = image.width;
+          canvas.height = image.height;
+          const context = canvas.getContext("2d");
+          context.drawImage(image, 0, 0, image.width, image.height);
+          //得到图片的base64编码数据
+          const url = canvas.toDataURL("image/png");
+          // 生成一个 a 标签
+          const a = document.createElement("a");
+          // 创建一个点击事件
+          const event = new MouseEvent("click");
+          // 将 a 的 download 属性设置为我们想要下载的图片的名称，若 name 不存在则使用'图片'作为默认名称
+          a.download = name || "图片";
+          // 将生成的 URL 设置为 a.href 属性
+          a.href = url;
+          // 触发 a 的点击事件
+          a.dispatchEvent(event);
+          // return a;
+        };
+        image.src = src;
     }
 
     render() {
@@ -59,7 +90,11 @@ class ImageDetail extends React.Component {
                             </div>
                         </div>
                         <div className={Style.DownloadButton}>
-                            <Button type="primary" shape="round" icon={<DownloadOutlined />} size='large'>
+                            <Button type="primary" 
+                                    shape="round" 
+                                    icon={<DownloadOutlined />}
+                                    size='large' 
+                                    onClick={this.downloadImage.bind(this, imageInfo.imageSrc, imageInfo.imageName)}>
                                 Download
                             </Button>
                         </div>
